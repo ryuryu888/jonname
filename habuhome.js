abf1070a9d1e25cd0a0426e29c038db327bc964d
ryuryu888 <109925184+ryuryu888@users.noreply.github.com>
2022-08-01 14:28:26 +0900

    Add files via upload

---
+++ habuhome.js
@@ -0,0 +1,10 @@
+
+$('h1').animate({
+  'fontSize': '128px'
+},5000)
+
+$('#itibu').animate({'left': '100%'})
+          .animate({'left': '40%'})
+          .animate({'fontSize': '96px'})
+          .animate({'left': '100%'})
+          .animate({'left': '0'})
